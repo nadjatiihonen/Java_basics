c0bdbbebe7e64a21e1b1df89f146ee7052e56f4b
nadjatiihonen <nadja.toroponina@gmail.com>
2026-04-28 20:09:45 +0300

    MyParent.java

---
+++ src/MyParent.java
@@ -0,0 +1,8 @@
+public class MyParent {
+    public static void main (String[] args){
+        System.out.println("Nadja");
+        System.out.println("Karin");
+        System.out.println("Katrin");
+        System.out.println("Denis");
+    }
+}
