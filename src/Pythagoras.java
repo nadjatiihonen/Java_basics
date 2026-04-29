51d2b8d462bbad26ec5d55f879cb3e4d942aec46
nadjatiihonen <nadja.toroponina@gmail.com>
2026-04-29 22:17:51 +0300

    Square / float

---
+++ src/Pythagoras.java
@@ -0,0 +1,5 @@
+public class Pythagoras {
+    static void main() {
+
+    }
+}
---
+++ src/Square.java
@@ -0,0 +1,8 @@
+public class Square {
+    static void main() {
+        float r = 10.8f;
+        float pi = 3.14f;
+        float s = pi * r * r;
+        System.out.println("area of a circle is " + s);
+    }
+}
