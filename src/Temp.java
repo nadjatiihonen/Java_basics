dadb5c540a47826e7f18a8ac7cfa86a537b7552c
nadjatiihonen <nadja.toroponina@gmail.com>
2026-04-29 18:17:33 +0300

    Temp / if, else

---
+++ src/Temp.java
@@ -0,0 +1,13 @@
+public class Temp {
+   public static void main(String[] args) {
+        int temp = 21;
+        if (temp > 25) {
+            System.out.println("Status: The air conditioner is on.");
+        } else if (temp < 22) {
+            System.out.println("Status: The air conditioner is off.");
+        } else {
+            System.out.println("Status: The air conditioner is inactive.");
+        }
+    }
+
+}
