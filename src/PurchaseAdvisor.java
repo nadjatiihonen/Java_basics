a1e4480225f12db4ca21429d17507303f4841e93
nadjatiihonen <nadja.toroponina@gmail.com>
2026-04-29 21:22:25 +0300

    PurchaseAdvisor

---
+++ src/PurchaseAdvisor.java
@@ -0,0 +1,11 @@
+public class PurchaseAdvisor {
+    public static void main(String[] args) {
+        int money = 99;
+        if (money > 500){
+            System.out.println("Pizza");
+        } else if (money < 100) {
+            System.out.println("Noodles");
+        }
+    }
+
+}
